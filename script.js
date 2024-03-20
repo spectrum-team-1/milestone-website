ff6ed81342675bb30cb5510bba7647e4bfdcf4ad
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-03-20 17:48:45 -0400

    Add buttons for selecting milestones

---
+++ script.js
@@ -0,0 +1,70 @@
+const MILESTONE_CONTAINER = document.getElementById('milestone-output');
+
+const milestoneHtml = {
+	one: `
+    <div id="milestone-container" class="milestone-container">
+        <div>
+            <iframe
+                class="embed"
+                src="https://www.youtube.com/embed/rbINuRxOAAQ">
+            </iframe>
+        </div>
+        <div class="download-container">
+            <span class="download-title">Download Links:</span>
+            <a
+                href="https://drive.google.com/uc?export=download&id=19UKQFrvncfl0qvkw_7bPapGw_ztRVRr9"
+                target="_blank"
+                class="download button"
+                >Milestone One</a
+            >
+        </div>
+    </div>
+    `,
+	two: `
+        <div id="milestone-container"  class="milestone-container">
+            <div>
+                <iframe
+                    class="embed"
+                    src="https://www.youtube.com/embed/rbINuRxOAAQ">
+                </iframe>
+            </div>
+            <div class="download-container">
+                <span class="download-title">Download Links:</span>
+                <a
+                    href="https://drive.google.com/uc?export=download&id=19UKQFrvncfl0qvkw_7bPapGw_ztRVRr9"
+                    target="_blank"
+                    class="download button"
+                    >Milestone Two</a
+                >
+            </div>
+        </div>
+    `,
+};
+
+MILESTONE_CONTAINER.insertAdjacentHTML('beforebegin', milestoneHtml.one);
+
+function updateMilestone(button, value) {
+	removeOutput();
+	clearSelectedButton();
+
+	switch (value) {
+		case 'one':
+			MILESTONE_CONTAINER.insertAdjacentHTML('beforebegin', milestoneHtml.one);
+			break;
+		case 'two':
+			MILESTONE_CONTAINER.insertAdjacentHTML('beforebegin', milestoneHtml.two);
+			break;
+	}
+
+	button.classList.add('selected');
+}
+
+function removeOutput() {
+	const output = document.getElementById('milestone-container');
+	output.remove();
+}
+
+function clearSelectedButton() {
+	const element = document.querySelector('.selected');
+	element.classList.remove('selected');
+}
