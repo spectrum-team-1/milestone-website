ef7dfe8cf6df804aae7017bdb5724299aed85628
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-03-20 19:47:14 -0400

    formatting

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const MILESTONE_CONTAINER = document.getElementById('milestone-output');
 
 const milestoneHtml = {
 	one: `
-    <div id="milestone-container" class="milestone-container fade-in">
-        <div>
-            <iframe
-                class="embed"
-                src="https://www.youtube.com/embed/rbINuRxOAAQ">
-            </iframe>
+        <div id="milestone-container" class="milestone-container fade-in">
+            <div>
+                <iframe
+                    class="embed"
+                    src="https://www.youtube.com/embed/rbINuRxOAAQ">
+                </iframe>
+            </div>
+            <div class="download-container">
+                <span class="download-title">Download Deliverables:</span>
+                <a
+                    href="https://drive.google.com/uc?export=download&id=19UKQFrvncfl0qvkw_7bPapGw_ztRVRr9"
+                    target="_blank"
+                    class="download button"
+                    >Milestone One</a
+                >
+            </div>
         </div>
-        <div class="download-container">
-            <span class="download-title">Download Deliverables:</span>
-            <a
-                href="https://drive.google.com/uc?export=download&id=19UKQFrvncfl0qvkw_7bPapGw_ztRVRr9"
-                target="_blank"
-                class="download button"
-                >Milestone One</a
-            >
-        </div>
-    </div>
     `,
 	two: `
         <div id="milestone-container"  class="milestone-container fade-in">
